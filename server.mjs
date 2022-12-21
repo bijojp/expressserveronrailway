cb91dc8ed8ee60c47c3bcfa622e40f44c6450f0f
Bijo <bijo.panicker@cuemath.com>
2022-12-21 16:48:21 +0530

    changed order of cors

--- server.mjs
+++ server.mjs
@@ -9,17 +9,20 @@ import mongoose from 'mongoose';
 
 import cors from 'cors'
 
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions)) 
 
 
 const app = express();
 app.use(express.json());
+
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+ app.use(cors(corsOptions)) 
+ 
 const port = process.env.PORT || 3000 ;
 
 
